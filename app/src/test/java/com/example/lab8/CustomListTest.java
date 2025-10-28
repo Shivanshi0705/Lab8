4747b7c1dcf8ee4315b1c5b2c5f13e6b27abe2f0
Shivanshi0705 <smakkar@ualberta.ca>
2025-10-27 23:24:29 -0600

    implement hasCity to fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -21,4 +21,19 @@ public class CustomListTest {
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(), listSize + 1);
     }
+    @org.junit.Test
+
+    public void hasCityTest() {
+        list = MockCityList();
+        City estevan = new City("Estevan", "SK");
+        City redDeer = new City("Red Deer", "AB");
+
+        list.addCity(estevan);
+
+        // should find an existing city
+        org.junit.Assert.assertTrue(list.hasCity(new City("Estevan", "SK")));
+
+        // should not find a city we never added
+        org.junit.Assert.assertFalse(list.hasCity(redDeer));
+    }
 }
