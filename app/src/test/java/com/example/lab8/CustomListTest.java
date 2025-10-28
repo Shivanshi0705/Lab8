1f4a118f506c7d08cf5f67e349d5f7a3c2dd079c
Shivanshi0705 <smakkar@ualberta.ca>
2025-10-27 22:26:38 -0600

    passing addCity test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -10,10 +10,12 @@ public class CustomList {
         this.cities = cities;
     }
 
+    /** this gets size of the list */
     public int getCount() {
         return cities.size();
     }
 
+    /** this adds a city object to the list */
     public void addCity(City city) {
         cities.add(city);
     }
