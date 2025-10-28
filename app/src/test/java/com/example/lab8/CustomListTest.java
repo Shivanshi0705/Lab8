accf237649476a34045b2070e9d215f080b5dab3
Shivanshi0705 <smakkar@ualberta.ca>
2025-10-27 22:09:52 -0600

    add failing unit test for addCity

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,24 @@
 package com.example.lab8;
 
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+    public CustomList MockCityList() {
+        // Match your CustomList constructor used in the lab
+        list = new CustomList(null, new ArrayList<City>());
+        return list;
+    }
 
+    @Test
+    public void addCityTest() {
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 }
